6a6dcd4f8d9392cc1f28da54cf0d0cf1c554d3d9
AmmarKOfficial <AmmarKOfficial@gmail.com>
2024-02-14 22:53:17 +0500

    Project Id

--- src/client.js
+++ src/client.js
@@ -2,11 +2,11 @@ import {createClient} from '@sanity/client'
 import imageUrlBuilder from '@sanity/image-url'
 
 export const client = createClient({
-    projectId: import.meta.env.VITE_PROJECT_ID,
+    projectId: 'ro78egwa',
     dataset: 'production',
     useCdn: true, // set to `false` to bypass the edge cache
-    apiVersion: import.meta.env.VITE_API_VERSION, // use current date (YYYY-MM-DD) to target the latest API version
-    token: import.meta.env.VITE_REACT_APP_SANITY_TOKEN // Only if you want to update content with the client
+    apiVersion: '2023-05-03', // use current date (YYYY-MM-DD) to target the latest API version
+    token: 'skjcqMLXMl2VrPBE6g0GB9m2KwOupNpmKHGUzzjtbFJCWwxRP7nCnF7loAmQnuTI82ubZtW2hUAUn6rO3Cs5P9rB5xcg1bkvbSKoWZBXzkPRFlGgt5IetgDpQJmRLbThbXEhk6aOyTNe7Cu5AKUml4wm4L0JrSHZ1EprtUFEubdf9nGUfeMd' // Only if you want to update content with the client
   })
 
 
